ec60accf94e439abdb76c55768a65491c0140887
tounekti <dtounakty@gmail.com>
2025-02-13 04:58:37 +0100

    mes entités

--- src/Entity/Paiement.php
+++ src/Entity/Paiement.php
@@ -13,8 +13,75 @@ class Paiement
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(type: 'float')]
+    private float $montant;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $datePaiement;
+
+    #[ORM\Column(length: 255)]
+    private string $methode;
+
+    #[ORM\OneToOne(targetEntity: Commande::class, inversedBy: 'paiement')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Commande $commande = null;
+
+    public function __construct()
+    {
+        $this->datePaiement = new \DateTime();  // Par défaut, la date de paiement est la date actuelle
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    public function getMontant(): float
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(float $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getDatePaiement(): \DateTime
+    {
+        return $this->datePaiement;
+    }
+
+    public function setDatePaiement(\DateTime $datePaiement): self
+    {
+        $this->datePaiement = $datePaiement;
+
+        return $this;
+    }
+
+    public function getMethode(): string
+    {
+        return $this->methode;
+    }
+
+    public function setMethode(string $methode): self
+    {
+        $this->methode = $methode;
+
+        return $this;
+    }
+
+    public function getCommande(): ?Commande
+    {
+        return $this->commande;
+    }
+
+    public function setCommande(?Commande $commande): self
+    {
+        $this->commande = $commande;
+
+        return $this;
+    }
 }
+
--- src/Entity/Panier.php
+++ src/Entity/Panier.php
@@ -13,8 +13,95 @@ class Panier
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $dateCreation;
+
+    #[ORM\Column(length: 255)]
+    private string $statut;
+
+    #[ORM\ManyToOne(targetEntity: Apprenant::class, inversedBy: 'paniers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Apprenant $apprenant = null;
+
+    #[ORM\OneToMany(mappedBy: 'panier', targetEntity: Cours::class)]
+    private iterable $cours;
+
+    #[ORM\OneToOne(mappedBy: 'panier', targetEntity: Commande::class)]
+    private ?Commande $commande = null;
+
+    public function __construct()
+    {
+        $this->dateCreation = new \DateTime();  // Par défaut, la date de création est la date actuelle
+        $this->statut = 'en attente';  // Par défaut, le statut peut être 'en attente'
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getDateCreation(): \DateTime
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTime $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getStatut(): string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getApprenant(): ?Apprenant
+    {
+        return $this->apprenant;
+    }
+
+    public function setApprenant(?Apprenant $apprenant): self
+    {
+        $this->apprenant = $apprenant;
+
+        return $this;
+    }
+
+    public function getCours(): iterable
+    {
+        return $this->cours;
+    }
+
+    public function setCours(iterable $cours): self
+    {
+        $this->cours = $cours;
+
+        return $this;
+    }
+
+    public function getCommande(): ?Commande
+    {
+        return $this->commande;
+    }
+
+    public function setCommande(?Commande $commande): self
+    {
+        $this->commande = $commande;
+
+        // Set (or unset) the owning side of the relation if necessary
+        if ($commande !== null && $commande->getPanier() !== $this) {
+            $commande->setPanier($this);
+        }
+
+        return $this;
     }
 }
