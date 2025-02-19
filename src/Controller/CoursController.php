782fa03e61e97596557136460bc432fd9d6934b4
Aroussi <benaribiaaroussi@gmail.com>
2025-02-19 23:25:18 +0100

    updated design for each form

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CoursType;
 
 #[Route('/cours')]
 final class CoursController extends AbstractController
--- src/Repository/CoursRepository.php
+++ src/Repository/CoursRepository.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Repository;
 
 use App\Entity\Cours;
@@ -16,28 +17,23 @@ class CoursRepository extends ServiceEntityRepository
         parent::__construct($registry, Cours::class);
     }
 
-    //    /**
-    //     * @return Cours[] Returns an array of Cours objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    // Add the save method here
+    public function save(Cours $entity, bool $flush = false): void
+    {
+        $this->_em->persist($entity); // Persist the entity
 
-    //    public function findOneBySomeField($value): ?Cours
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        if ($flush) {
+            $this->_em->flush(); // If flush is true, save the entity to the database
+        }
+    }
+
+    // Optionally, add a remove method as well if you want
+    public function remove(Cours $entity, bool $flush = false): void
+    {
+        $this->_em->remove($entity); // Remove the entity
+
+        if ($flush) {
+            $this->_em->flush(); // If flush is true, delete the entity from the database
+        }
+    }
 }
